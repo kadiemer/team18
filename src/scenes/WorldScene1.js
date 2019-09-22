b332cb569db70e4123cb320d8b521eb224eb85a1
Daniel Nelson <daniel.h.nelson@att.net>
2019-09-22 07:57:41 -0500

    Update WorldScene1.js

--- src/scenes/WorldScene1.js
+++ src/scenes/WorldScene1.js
@@ -14,7 +14,7 @@ export default class WorldScene1 extends Phaser.Scene {
     this.load.image("tiles", "./assets/tilesets/tuxmon-sample-32px-extruded.png");
     this.load.tilemapTiledJSON("map", "./assets/tilemaps/tuxemon-town.json");
     this.load.image("zombie", "./assets/sprites/zombieSprite.png");
-    this.load.image("guy", "./assets/sprites/guy.png");
+    this.load.image("guy", "./assets/sprites/guySprite.png");
 
     // An atlas is a way to pack multiple images together into one texture. I'm using it to load all
     // the player animations (walking left, walking right, etc.) in one image. For more info see:
@@ -55,9 +55,11 @@ export default class WorldScene1 extends Phaser.Scene {
     // Create a sprite with physics enabled via the physics system. The image used for the sprite has
     // a bit of whitespace, so I'm using setSize & setOffset to control the size of the player's body.
     this.player = this.physics.add
-      .sprite(spawnPoint.x, spawnPoint.y, "atlas", "misa-front")
+      .sprite(spawnPoint.x, spawnPoint.y, "guy")
       .setSize(30, 40)
       .setOffset(0, 24);
+
+    this.player.scale = .1;
 
     // Watch the player and worldLayer for collisions, for the duration of the scene:
     //this.physics.add.collider(this.player, worldLayer);
@@ -69,51 +71,6 @@ export default class WorldScene1 extends Phaser.Scene {
 
     // Create the player's walking animations from the texture atlas. These are stored in the global
     // animation manager so any sprite can access them.
-    const anims = this.anims;
-    anims.create({
-      key: "misa-left-walk",
-      frames: anims.generateFrameNames("atlas", {
-        prefix: "misa-left-walk.",
-        start: 0,
-        end: 3,
-        zeroPad: 3
-      }),
-      frameRate: 10,
-      repeat: -1
-    });
-    anims.create({
-      key: "misa-right-walk",
-      frames: anims.generateFrameNames("atlas", {
-        prefix: "misa-right-walk.",
-        start: 0,
-        end: 3,
-        zeroPad: 3
-      }),
-      frameRate: 10,
-      repeat: -1
-    });
-    anims.create({
-      key: "misa-front-walk",
-      frames: anims.generateFrameNames("atlas", {
-        prefix: "misa-front-walk.",
-        start: 0,
-        end: 3,
-        zeroPad: 3
-      }),
-      frameRate: 10,
-      repeat: -1
-    });
-    anims.create({
-      key: "misa-back-walk",
-      frames: anims.generateFrameNames("atlas", {
-        prefix: "misa-back-walk.",
-        start: 0,
-        end: 3,
-        zeroPad: 3
-      }),
-      frameRate: 10,
-      repeat: -1
-    });
 
     const camera = this.cameras.main;
     camera.startFollow(this.player);
@@ -196,25 +153,5 @@ export default class WorldScene1 extends Phaser.Scene {
     // Normalize and scale the velocity so that player can't move faster along a diagonal
     this.player.body.velocity.normalize().scale(speed);
 
-    // Update the animation last and give left/right animations precedence over up/down animations
-    if (this.cursors.left.isDown) {
-      this.player.anims.play("misa-left-walk", true);
-    } else if (this.cursors.right.isDown) {
-      this.player.anims.play("misa-right-walk", true);
-    } else if (this.cursors.up.isDown) {
-      this.player.anims.play("misa-back-walk", true);
-    } else if (this.cursors.down.isDown) {
-      this.player.anims.play("misa-front-walk", true);
-    } else {
-      this.player.anims.stop();
-
-      // If we were moving, pick and idle frame to use
-      if (prevVelocity.x < 0) this.player.setTexture("atlas", "misa-left");
-      else if (prevVelocity.x > 0)
-        this.player.setTexture("atlas", "misa-right");
-      else if (prevVelocity.y < 0) this.player.setTexture("atlas", "misa-back");
-      else if (prevVelocity.y > 0)
-        this.player.setTexture("atlas", "misa-front");
-    }
   }
 }
