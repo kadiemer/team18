44b79d21bb41d739fa6fd800ab08a504412ddfc8
Samir <samirpunjwany@gmail.com>
2019-09-23 13:18:34 -0500

    Menu Scene added

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@
 import BootScene from './scenes/BootScene.js';
 import Config from './config/config.js';
 import WorldScene1 from './scenes/WorldScene1.js';
+import MenuScene from './scenes/MenuScene.js';
 
 class Game extends Phaser.Game {
   constructor () {
     super(Config);
     this.scene.add('Boot', BootScene);
     this.scene.add('WorldScene1', WorldScene1);
-    this.scene.start('WorldScene1');
+    this.scene.add('MenuScene', MenuScene);
+    this.scene.start('MenuScene');
   }
 }
 
---
+++ src/scenes/MenuScene.js
@@ -0,0 +1,42 @@
+/*global Phaser*/
+import * as ChangeScene from './ChangeScene.js';
+import WorldScene1 from './WorldScene1.js'
+export default class MenuScene extends Phaser.Scene {
+  constructor () {
+    super('MenuScene');
+  }
+
+  init (data) {
+    // Initialization code goes here
+  }
+
+  preload () {
+    // Preload assets
+    this.load.image('menu', './assets/images/menuscreen.png');
+
+    // Declare variables for center of the scene
+    this.centerX = this.cameras.main.width / 2;
+    this.centerY = this.cameras.main.height / 2;
+  }
+
+  create (data) {
+    //Add change scene event listeners
+    ChangeScene.addSceneEventListeners(this);
+    //Create the scene
+    var background = this.add.image(400, 300, 'menu');
+
+    var play = this.add.text(300, 195, '< play >',
+    {fontFamily: 'Fantasy', fontSize: 50, color: '#ffffff'}).setInteractive();
+    play.on("pointerup", function() {
+      this.scene.start('WorldScene1');
+    }, this
+  );
+
+    var title = this.add.text(220,70, '[ Dance The Bite Away ]',
+    {fontFamily: 'Fantasy', fontSize: 50, color: '#ffffff'});
+
+  }
+
+  update (time, delta) {
+  }
+}
