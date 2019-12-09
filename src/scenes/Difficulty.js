bb9089363001f09e6477609cee79afc03c3a11bc
Kadie Meroney <kadiemeroney@gmail.com>
2019-12-09 17:40:05 -0600

    changed back

--- src/scenes/Difficulty.js
+++ src/scenes/Difficulty.js
@@ -44,9 +44,9 @@ export default class WinScene extends Phaser.Scene {
       });
 
 
-    var play = this.add.text(275, 150, 'CHOOSE DIFFICULTY', {
+    var play = this.add.text(150, 150, 'CHOOSE DIFFICULTY', {
       fontFamily: 'League Gothic',
-      fontSize: 275,
+      fontSize: 150,
       color: '#e0dac3',
       stroke: "#000000",
       strokeThickness: 25
--- src/scenes/LoseScene.js
+++ src/scenes/LoseScene.js
@@ -26,12 +26,12 @@ export default class LoseScene extends Phaser.Scene {
     //tell user game is over and their score
     var enterKey = this.input.keyboard.addKey(Phaser﻿.Input.Keyboard.KeyCodes.ENTER);
 
-    var text = this.add.text( 450, 250, "Oh no, you died!",
-    {fontFamily: 'League Gothic', fontSize: 200, color: '#e0dac3'});
-    var play = this.add.text(580, 750, 'Press enter to try again',
-    {fontFamily: 'League Gothic', fontSize: 100, color: '#ab0000'});
-    var score = this.add.text(460, 600,'You turned ' + window.transformedCount + '/' + window.maxZombies + ' zombies human',
-    {fontFamily: 'League Gothic', fontSize: 100, color: '#ab0000'});
+    var text = this.add.text( 550, 250, "Oh no, you died!",
+    {fontFamily: 'League Gothic', fontSize: 150, color: '#e0dac3'});
+    var play = this.add.text(680, 750, 'Press enter to try again',
+    {fontFamily: 'League Gothic', fontSize: 75, color: '#ab0000'});
+    var score = this.add.text(560, 600,'You turned ' + window.transformedCount + '/' + window.maxZombies + ' zombies human',
+    {fontFamily: 'League Gothic', fontSize: 75, color: '#ab0000'});
 
 
     enterKey.on("down", function() {
